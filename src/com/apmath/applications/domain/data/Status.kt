58557d9d7957c26152f47e43feb0d4722e5073a3
evolyuta <dima.97_97@mail.ru>
2019-05-13 22:58:02 +0300

    create data Currency, Money, Status

---
+++ src/com/apmath/applications/domain/data/Status.kt
@@ -0,0 +1,14 @@
+package com.apmath.applications.domain.data
+
+import com.google.gson.annotations.SerializedName
+
+enum class Status{
+
+    @SerializedName("approved")
+    APPROVED,
+    @SerializedName("rejected")
+    REJECTED,
+    @SerializedName("pending")
+    PENDING
+
+}
