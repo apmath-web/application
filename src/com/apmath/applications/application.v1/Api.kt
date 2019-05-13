c5335467ff8c9a67658a2550b080f146b822b614
evolyuta <dima.97_97@mail.ru>
2019-05-13 22:57:16 +0300

    change Api.kt

--- src/com/apmath/applications/application.v1/Api.kt
+++ src/com/apmath/applications/application.v1/Api.kt
@@ -1,10 +1,20 @@
 package com.apmath.applications.application.v1
 
+import com.apmath.applications.application.v1.actions.v1Create
 import com.apmath.applications.application.v1.actions.v1Info
+import com.apmath.applications.domain.exceptions.ApiException
+import com.apmath.applications.domain.services.ApplicationServiceInterface
+import com.sun.xml.internal.messaging.saaj.soap.SOAPIOException
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
+import io.ktor.util.error
+import org.koin.ktor.ext.inject
 
 internal fun Routing.v1() {
 
@@ -14,9 +24,25 @@ internal fun Routing.v1() {
 
 private fun Routing.v1Info() {
 
+    val applicationService: ApplicationServiceInterface by inject()
+
     route("v1") {
         get("info") {
             call.v1Info()
         }
+        post("{clientId}") {
+            call.v1Create(applicationService)
+        }
     }
+}
+
+suspend fun ApplicationCall.respondError(e: Exception) {
+
+    application.environment.log.error(e)
+    if (e is ApiException) {
+        respond(e.code, e.message)
+    } else {
+        respond(HttpStatusCode.InternalServerError, "Something went wrong")
+    }
+
 }
