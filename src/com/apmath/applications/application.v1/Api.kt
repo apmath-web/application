3ebfeaf0282c163d01a82df1b0f6f7aeb479f819
malinink <apmath@malinink.ru>
2019-05-18 22:18:58 +0300

    Add route got loan accept request

--- src/com/apmath/applications/application.v1/Api.kt
+++ src/com/apmath/applications/application.v1/Api.kt
@@ -1,15 +1,18 @@
 package com.apmath.applications.application.v1
 
+import com.apmath.applications.application.v1.actions.v1LoanRequest
 import com.apmath.applications.application.v1.actions.v1Info
 import io.ktor.application.call
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 
 internal fun Routing.v1() {
 
     v1Info()
     // other route groups here
+    v1Application()
 }
 
 private fun Routing.v1Info() {
@@ -17,6 +20,16 @@ private fun Routing.v1Info() {
     route("v1") {
         get("info") {
             call.v1Info()
+        }
+    }
+}
+
+private fun Routing.v1Application() {
+
+    route("v1") {
+        post("{clientId}/{applicationId}") {
+            val parameters = call.parameters
+            call.v1LoanRequest(parameters["clientId"]!!, parameters["applicationId"]!!)
         }
     }
 }
