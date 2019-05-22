3130a517ec01e6d5dcbb2d1c8dbc796a7d86d8ee
evolyuta <dima.97_97@mail.ru>
2019-05-22 22:02:28 +0300

    change imports

--- src/com/apmath/applications/domain/services/ApplicationServiceInterface.kt
+++ src/com/apmath/applications/domain/services/ApplicationServiceInterface.kt
@@ -1,10 +1,10 @@
 package com.apmath.applications.domain.services
 
-import com.apmath.applications.application.v1.models.Application
+import com.apmath.applications.domain.models.applications.ApplicationCreationDataInterface
 import com.apmath.applications.domain.models.applications.ApplicationInterface
 
 interface ApplicationServiceInterface{
 
-    suspend fun add(application: ApplicationInterface): Int
+    suspend fun add(application: ApplicationCreationDataInterface): Int
 
 }
