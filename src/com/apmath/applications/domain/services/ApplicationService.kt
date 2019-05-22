564e57cb8446e14d42f357e0215a54489fdba68b
evolyuta <dima.97_97@mail.ru>
2019-05-22 22:02:05 +0300

    delete empty rows

--- src/com/apmath/applications/domain/services/ApplicationService.kt
+++ src/com/apmath/applications/domain/services/ApplicationService.kt
@@ -1,15 +1,12 @@
 package com.apmath.applications.domain.services
 
+import com.apmath.applications.domain.models.applications.ApplicationCreationData
+import com.apmath.applications.domain.models.applications.ApplicationCreationDataInterface
 import com.apmath.applications.domain.models.applications.ApplicationInterface
 
-class ApplicationService(
-
-
-
-) : ApplicationServiceInterface{
-    override suspend fun add(application: ApplicationInterface): Int {
+class ApplicationService() : ApplicationServiceInterface{
+    override suspend fun add(application: ApplicationCreationDataInterface): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 
 }
