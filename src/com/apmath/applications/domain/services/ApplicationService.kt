354ec95b67ddcaf99cc2561093b5f2833be26ce8
evolyuta <dima.97_97@mail.ru>
2019-05-27 02:22:06 +0300

    add BL

--- src/com/apmath/applications/domain/services/ApplicationService.kt
+++ src/com/apmath/applications/domain/services/ApplicationService.kt
@@ -1,20 +1,23 @@
 package com.apmath.applications.domain.services
 
 import com.apmath.application.domain.exceptions.NoClientException
-import com.apmath.applications.domain.models.applications.Application
-import com.apmath.applications.domain.models.applications.ApplicationCreationData
-import com.apmath.applications.domain.models.applications.ApplicationCreationDataInterface
-import com.apmath.applications.domain.models.applications.toApplication
-import com.apmath.applications.domain.models.applications.ApplicationInterface
+import com.apmath.applications.domain.data.Status
+import com.apmath.applications.domain.models.applications.*
 import com.apmath.applications.domain.repositories.RepositoryInterface
+import com.apmath.applications.infrastructure.ApplicationDetails
 import com.apmath.applications.infrastructure.applications
 import com.apmath.applications.infrastructure.fetchers.ClientsFetcher
+import com.apmath.applications.infrastructure.fetchers.ExpensesFetcher
+import com.apmath.applications.infrastructure.fetchers.InterestsFetcher
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 
 class ApplicationService(
     private val clientsFetcher: ClientsFetcher,
+    private val interestsFetcher: InterestsFetcher,
+    private val expensesFetcher: ExpensesFetcher,
     private val repository: RepositoryInterface
 ) : ApplicationServiceInterface {
     override suspend fun add(application: ApplicationCreationDataInterface): Int {
@@ -24,7 +27,18 @@ class ApplicationService(
             clientsFetcher.isExists(clientId)
         }
 
+        val interestsResult = GlobalScope.async(start = CoroutineStart.LAZY) {
+            val applicationInit = application.toApplicationInitialization()
+            interestsFetcher.initialization(applicationInit)
+        }
+
+        val expensesResult = GlobalScope.async(start = CoroutineStart.LAZY) {
+            val applicationInit = application.toApplicationInitialization()
+            expensesFetcher.initialization(applicationInit)
+        }
+
         val client = clientResult.await()
+        val applicationDetails = expensesResult.await()
 
         when {
             //client does not exists
@@ -33,7 +47,31 @@ class ApplicationService(
 
             else -> {
 
-                val applicationEmployee = application.toApplication()
+                var term = 12
+                val interests = 12
+                val expenses = expensesResult.await()
+                val maxAllowedAmount = expenses.maxAllowedAmount.toInt()
+                var minTermForMaxAmount = 1
+                var minTermForRequestedAmount = 1
+                val requestedAmount = application.amount
+                val status = Status.APPROVED
+
+                if (clientId % 2 == 0) {
+                    status != Status.APPROVED
+                } else {
+                    status != Status.REJECTED
+                }
+
+                minTermForMaxAmount = maxAllowedAmount / 200
+                minTermForRequestedAmount = requestedAmount.toInt() / 200
+                if (requestedAmount < maxAllowedAmount) {
+                    term = minTermForRequestedAmount*(1+interests)
+                } else {
+                    term = minTermForMaxAmount*(1+interests)
+                }
+
+
+                val applicationEmployee = application.toApplication(applicationDetails)
 
                 repository.store(applicationEmployee)
                 return applicationEmployee.id!!
