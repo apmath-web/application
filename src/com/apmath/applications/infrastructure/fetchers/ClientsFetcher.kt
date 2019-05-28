adb47600f07355a57da41840b6a29c95a10f6701
evolyuta <dima.97_97@mail.ru>
2019-05-29 02:54:28 +0300

    change get

--- src/com/apmath/applications/infrastructure/fetchers/ClientsFetcher.kt
+++ src/com/apmath/applications/infrastructure/fetchers/ClientsFetcher.kt
@@ -1,15 +1,22 @@
 package com.apmath.applications.infrastructure.fetchers
 
 import com.apmath.applications.domain.fetchers.ClientsFetcherInterface
+import io.ktor.client.request.request
+import io.ktor.client.request.url
 import io.ktor.client.response.HttpResponse
+import io.ktor.http.HttpMethod
 import io.ktor.http.isSuccess
 
 class ClientsFetcher(
     host: String,
     port: Int
 ) : AbstractFetcher(host, port), ClientsFetcherInterface {
+
+    suspend fun getClient (request: Int): HttpResponse {
+        return get("/v1/$request")
+    }
+
     override suspend fun isExists(clientId: Int): Boolean {
-        return get<HttpResponse>("/v1/$clientId")
-            .status.isSuccess()
+        return getClient(clientId).status.isSuccess()
     }
 }
