cc11aabcf66e5ddebe543e8bca934d41d9e7a909
evolyuta <dima.97_97@mail.ru>
2019-05-28 05:45:17 +0300

    add ExpensesFetcher

---
+++ src/com/apmath/applications/infrastructure/fetchers/ExpensesFetcher.kt
@@ -0,0 +1,14 @@
+package com.apmath.applications.infrastructure.fetchers
+
+import com.apmath.applications.domain.fetchers.ExpensesFetcherInterface
+import com.apmath.applications.domain.models.applications.ApplicationInitializationInterface
+import com.apmath.applications.infrastructure.ApplicationDetails
+
+class ExpensesFetcher(
+    host: String,
+    port: Int
+) : AbstractFetcher(host, port), ExpensesFetcherInterface {
+    override suspend fun initialization(application: ApplicationInitializationInterface): ApplicationDetails {
+        return post("/v1/application", application)
+    }
+}
