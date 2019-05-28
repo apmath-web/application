8c7a67bd09061827c7f309fab3b73dabbd0cb083
evolyuta <dima.97_97@mail.ru>
2019-05-28 05:21:10 +0300

    add ApplicationDetails

---
+++ src/com/apmath/applications/domain/models/applications/ApplicationDetailsInterface.kt
@@ -0,0 +1,14 @@
+package com.apmath.applications.domain.models.applications
+
+import com.apmath.applications.domain.data.Money
+import com.apmath.applications.domain.data.Status
+
+interface ApplicationDetailsInterface {
+    val interest: Int
+    val maxPayment: Money
+    val maxAllowedAmount: Money
+    val minTermForMaxAmount: Money
+    val minTermForRequestedAmount: Money
+    val requestedAmount: Money
+    val status: Status
+}
