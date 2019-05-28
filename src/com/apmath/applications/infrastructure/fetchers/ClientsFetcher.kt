4eec2b64af2a4cb87ceb7693d384cf7ab6861b9c
evolyuta <dima.97_97@mail.ru>
2019-05-28 05:45:06 +0300

    add ClientsFetcher

---
+++ src/com/apmath/applications/infrastructure/fetchers/ClientsFetcher.kt
@@ -0,0 +1,15 @@
+package com.apmath.applications.infrastructure.fetchers
+
+import com.apmath.applications.domain.fetchers.ClientsFetcherInterface
+import io.ktor.client.response.HttpResponse
+import io.ktor.http.isSuccess
+
+class ClientsFetcher(
+    host: String,
+    port: Int
+) : AbstractFetcher(host, port), ClientsFetcherInterface {
+    override suspend fun isExists(clientId: Int): Boolean {
+        return get<HttpResponse>("/v1/$clientId")
+            .status.isSuccess()
+    }
+}
