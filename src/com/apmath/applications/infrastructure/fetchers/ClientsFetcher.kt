96a70ba087746712e7060666667fe2ea9f46e1be
evolyuta <dima.97_97@mail.ru>
2019-05-29 12:08:38 +0300

    fix name of parameter

--- src/com/apmath/applications/infrastructure/fetchers/ClientsFetcher.kt
+++ src/com/apmath/applications/infrastructure/fetchers/ClientsFetcher.kt
@@ -1,10 +1,7 @@
 package com.apmath.applications.infrastructure.fetchers
 
 import com.apmath.applications.domain.fetchers.ClientsFetcherInterface
-import io.ktor.client.request.request
-import io.ktor.client.request.url
 import io.ktor.client.response.HttpResponse
-import io.ktor.http.HttpMethod
 import io.ktor.http.isSuccess
 
 class ClientsFetcher(
@@ -12,8 +9,8 @@ class ClientsFetcher(
     port: Int
 ) : AbstractFetcher(host, port), ClientsFetcherInterface {
 
-    suspend fun getClient (request: Int): HttpResponse {
-        return get("/v1/$request")
+    suspend fun getClient(clientId: Int): HttpResponse {
+        return get("/v1/$clientId")
     }
 
     override suspend fun isExists(clientId: Int): Boolean {
