a69b14808f22c9865f55ae2916eba1276026414e
evolyuta <dima.97_97@mail.ru>
2019-05-29 04:03:34 +0300

    return interest, maxPayment

--- src/com/apmath/applications/infrastructure/ApplicationDetails.kt
+++ src/com/apmath/applications/infrastructure/ApplicationDetails.kt
@@ -2,10 +2,14 @@ package com.apmath.applications.infrastructure
 
 import com.apmath.applications.domain.data.Money
 import com.apmath.applications.domain.data.Status
+import com.apmath.applications.domain.fetchers.InterestsFetcherInterface
 import com.apmath.applications.domain.models.applications.Application
 import com.apmath.applications.domain.models.applications.ApplicationDetailsInterface
 
 class ApplicationDetails(
+
+    override val interest: Int,
+    override val maxPayment: Money,
 
     override val maxAllowedAmount: Money,
     override val minTermForMaxAmount: Money,
