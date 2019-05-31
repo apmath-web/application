15130e5d58f05811b37dbf6f5cf85a7227a84df8
evolyuta <dima.97_97@mail.ru>
2019-05-31 23:09:29 +0300

    add exceptions

--- src/com/apmath/applications/application/v1/exceptions/ApiException.kt
+++ src/com/apmath/applications/application/v1/exceptions/ApiException.kt
@@ -2,6 +2,7 @@ package com.apmath.applications.application.v1.exceptions
 
 import io.ktor.http.HttpStatusCode
 
-abstract class ApiException(status: HttpStatusCode, message: String) : Exception(message) {
-    val status: HttpStatusCode = status
-}
+abstract class ApiException(
+    message: String,
+    val status: HttpStatusCode
+) : Exception(message)
