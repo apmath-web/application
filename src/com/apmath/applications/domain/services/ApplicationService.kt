6cabf81613a60e068694f5b051f82f1033a716d6
evolyuta <dima.97_97@mail.ru>
2019-06-01 03:13:22 +0300

    add calculation into BL

--- src/com/apmath/applications/domain/services/ApplicationService.kt
+++ src/com/apmath/applications/domain/services/ApplicationService.kt
@@ -1,10 +1,10 @@
 package com.apmath.applications.domain.services
 
+import com.apmath.applications.domain.data.Money
 import com.apmath.applications.domain.exceptions.NoClientException
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import com.apmath.applications.domain.data.Status
-import com.apmath.applications.domain.exceptions.ForbiddenAccessException
 import com.apmath.applications.domain.fetchers.ClientsFetcherInterface
 import com.apmath.applications.domain.fetchers.ExpensesFetcherInterface
 import com.apmath.applications.domain.fetchers.InterestsFetcherInterface
@@ -30,25 +30,65 @@ class ApplicationService(
             interestsFetcher.initialization(applicationInit)
         }
 
-        val expensessResult = GlobalScope.async {
+        val expensesResult = GlobalScope.async {
             val applicationInit = application.toApplicationInitialization()
             expensesFetcher.initialization(applicationInit)
         }
 
         val client = clientsResult.await()
         val interest = interestsResult.await()
-        val expense = expensessResult.await()
+        val expense = expensesResult.await()
 
         if (client) {
+
+            fun calcucatePayment(interest: Int, term: Int, amount: Money): Money {
+                val interestMonth = interest.toDouble() / 12 / 100
+
+
+                val payment = (interestMonth * Math.pow(
+                    (1 + interestMonth),
+                    term.toDouble()
+                )) / (Math.pow(
+                    (1 + interestMonth),
+                    term.toDouble()
+                ) - 1) * amount
+
+                return payment.toLong()
+            }
+
+            fun calcucateTerm(interest: Int, payment: Money, amount: Money): Int {
+                val interestMonth = interest.toDouble() / 100 / 12
+
+                val interestPowTerm = amount * interestMonth / (amount - interestMonth * interestMonth * payment)
+
+                return (Math.log(interestPowTerm) / Math.log(interestMonth)).toInt()
+            }
 
             val interest = interest.interest
             val maxPayment = expense.maxPayment
 
-            var maxAllowedAmount = application.amount
-            var minTermForMaxAmount = 228
+            var maxAllowedAmount = application.amount*2
+            var minTermForMaxAmount = application.term
             var minTermForRequestedAmount = application.term
             var requestedAmount = application.amount
             var status = Status.APPROVED
+
+            var calcucatedPayment = calcucatePayment(interest, application.term, application.amount)
+
+            if (calcucatedPayment > maxPayment){
+                status = Status.REJECTED}
+            else {
+                status = Status.APPROVED
+
+                calcucatedPayment = calcucatePayment(interest, application.term, maxAllowedAmount)
+                println(calcucatedPayment)
+                if (calcucatedPayment < maxPayment) {
+                    minTermForMaxAmount = calcucateTerm(interest, maxPayment, maxAllowedAmount)
+                } else {
+                    maxAllowedAmount = application.amount
+                }
+
+            }
 
             val applicationDetails = ApplicationDetails(
                 interest,
